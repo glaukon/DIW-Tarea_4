d2677ac53ce5ebb131ea7779502aa1779fbb4e3c
glaukon <glaukonsete@gmail.com>
2022-01-13 12:15:22 +0100

    Primer commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <logo />
+  <centro />
+  <pie />
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import centro from "./components/centro.vue";
+import logo from "./components/logo.vue";
+import pie from "./components/pie.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    logo,
+    centro,
+    pie,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
 </style>
---
+++ src/components/logo.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+<div class="logo">
+    <div class="container">
+        <img src="../assets/logo.png" alt="Logo">
+      </div>
+</div>
+ <section class="container">
+    <nav class="menu">
+      <ul>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Prueba 1</a></li>
+        <li><a href="#">Prueba 2</a></li>
+      </ul>
+    </nav>
+    <div class="login">
+      <a class="registro" href="#">Registrate</a>
+      <a class="inicio_sesion" href="#">Iniciar Sesión</a>
+    </div>
+  </section>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+
+<style>
+.container{
+    /*background-color: aqua;*/
+}
+
+img{
+    
+   
+    display: flex;
+    flex-direction: row; 
+    height: 50px;
+    }
+
+</style>
