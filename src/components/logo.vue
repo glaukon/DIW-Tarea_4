a1252d9c23b21f2cb349343540943a0b166727be
glaukon <glaukonsete@gmail.com>
2022-01-18 13:22:39 +0100

    Menu y login todo OK

--- src/components/logo.vue
+++ src/components/logo.vue
@@ -17,9 +17,8 @@
         </ul>
       </nav>
       <div class="login">
-        <a href="#">Registro</a>
-
-        <a href="#">Iniciar Sesión</a>
+       <a href="#">Registro</a>
+       <a href="#">Iniciar Sesión</a>
       </div>
     </section>
   </div>
@@ -30,29 +29,25 @@
 
 
 <style>
- .barra_menu{
-  /* BARRA MENU COMPLETA*/
-  background-color: burlywood;
-  text-decoration-line: none;
+section{
+  background-color: green;
 }
-
 
 nav ul li{
   /* Menu Navegacion*/
   display: flex; 
   float: left; /* Hacemos que el menu se muestre horizontal */
   list-style-type: none; /* Quita los puntos de las listas */
-  justify-content: center;
+  
   
  
   
 }
 
-.menu ul li a {
+.menu  ul li a {
   text-decoration-line: none;
   margin: 10px;
   padding: 8px;
-  /*justify-content: center;*/
   
   
 
@@ -60,22 +55,26 @@ nav ul li{
 .login a {
   /* REGISTRO DE USUARIOS*/
 
-  display: flex;
-  flex-direction: row;
+
   text-decoration-line: none;
   margin: 10px;
   padding: 8px;
 }
-div .login {
+
+
+div.login {
   display: flex;
-  justify-content: flex-end;
+ justify-content:flex-end;
 
 
 
 }
 a:hover {
-  border: 1px solid black;
-  border-radius: 20px;
+  margin-block: auto ;
+  border: 2px solid white;
+  border-radius: 10px;
+
+
 }
 
 img {
